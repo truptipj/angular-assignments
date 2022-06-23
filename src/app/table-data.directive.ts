2551c8d1b39b50c0ea74546b9e52db59082ea3a9
truptipj <truptij@incubxperts.com>
2022-06-23 11:19:34 +0530

    "directive"

--- src/app/table-data.directive.ts
+++ src/app/table-data.directive.ts
@@ -1,22 +1,14 @@
-import { Directive,Renderer2, ElementRef, } from '@angular/core';
+import { Directive, ElementRef, } from '@angular/core';
 
 
 @Directive({
   selector: '[appTableData]'
 })
 export class TableDataDirective {
-
-  
-  constructor(  private renderer: Renderer2,
-    private el: ElementRef) {  
-    
-  }  
-  ngAfterViewInit(): void {  
-     
-     
-  
-    this.renderer.setStyle(this.el.nativeElement, 'text-decoration', 'underline');
-
-  }     
-}    
+  constructor(private el : ElementRef){
+    this.el.nativeElement.style.textDecoration = 'underline';
+    this.el.nativeElement.style.textDecorationColor = 'black';
+ 
+}   
+ }
 
